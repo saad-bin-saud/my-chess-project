1d549ee959492212481cbf4459ddc306362d8392
saad-bin-saud <oneofsaadsaud22@gmail.com>
2025-10-11 17:41:13 +0500

    n

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import AppleStyleGameMenu from './AppleStyleGameMenu'
+import AppleStyleGameMenu from './GameMenu'
 import './style.css'
 
 createRoot(document.getElementById('root')).render(
